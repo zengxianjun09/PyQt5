5a805be1638f7e683262dcac0f114e6dc00556d1
zengxianjun <1015920600@qq.com>
2018-01-25 15:53:06 +0800

    add subMenu

--- 1-25/status.py
+++ 1-25/status.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, qApp
+from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QMenu
 from PyQt5.QtGui import QIcon
 
 class Example(QMainWindow):
@@ -12,21 +12,21 @@ class Example(QMainWindow):
         
     def initUI(self):   
 
-        exitAct = QAction(QIcon('web.png'), '&Exit', self)
-        exitAct.setShortcut('Ctrl+Q')
-        exitAct.setStatusTip('Exit application')
-        exitAct.triggered.connect(qApp.quit)
-
-        #self.statusBar()
-
+        # 建立一级菜单 File
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(exitAct)
 
-        self.statusBar().showMessage('Ready go zxj') # 创建状态栏，在底部显示状态文本           
+        impMenu = QMenu('Import', self) # 新二级菜单名字叫Import
+        impAct = QAction('Import mail', self) # 建立二级菜单里面的动作 Import mail
+        impMenu.addAction(impAct) #把动作添加到二级菜单里面
+
+        newAct = QAction('New', self) # 一级菜单动作NEW
+
+        fileMenu.addAction(newAct) # 添加一级菜单里面的动作1
+        fileMenu.addMenu(impMenu) # 添加一级菜单里面的新菜单
         
         self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('StatusBar')    
+        self.setWindowTitle('Submenu')    
         self.show()
     
     
